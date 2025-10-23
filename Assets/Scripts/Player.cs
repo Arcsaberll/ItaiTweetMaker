3ab813419266de62428df7a00ba71dcf6a3cb629
Arcsaberll <birdman710yc@gmail.com>
2025-10-23 13:41:26 +0900

    Add player class with properties for player number and name

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,15 +2,27 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private int playerNum;
+    private string playerName;
+
+    public Player(int num, string name)
     {
-        
+        playerNum = num;
+        playerName = name;
     }
 
-    // Update is called once per frame
-    void Update()
+    public int GetPlayerNum()
     {
-        
+        return playerNum;
+    }
+
+    public string GetPlayerName()
+    {
+        return playerName;
+    }
+
+    public void SetPlayerName(string name)
+    {
+        playerName = name;
     }
 }
